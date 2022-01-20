dc69c6dfa1217bdee58134457ec84c57c02a96e9
mevo <mevlutodabas7@gmail.com>
2022-01-20 22:00:46 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+// console.log("I'm from file");
+
+// var x = 7;
+// var y = 5;
+
+// console.log({x});
+// console.log({y});
+// y = x;
+// console.log({y});
+// var alert = "Merhaba"
+// console.log(alert);
+//  alert("Hey")
+// var a = 'Global';
+// console.log(a);
+
+// {
+//     let a = 'Scope';
+//     console.log(a);
+//     {
+//         let a ='Inner Scope'
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+// console.log(a);
+// var c =3;
+// console.log(c);
+// var c = 5;
+// console.log(c);
+
+// const h = 'constant variable'
+// console.log(h);
+// h = '2'
+
+// console.log(d);
+// var d = 6;
+
+// console.log(e);
+// let e = 5;
+
+// var f;
+// console.log(typeof f);
+
+// f = 5;
+// console.log(typeof f);
+
+// f ='Merhaba ben string';
+// console.log(typeof f);
+
+// console.log(typeof 5);
+// console.log(typeof 0);
+// console.log(typeof 5.45);
+// console.log(typeof (0/1));
+// console.log(typeof NaN);
+
+// var merhaba = 'Merhaba'
+// var selam ='Sana da selam \'Mark'
+
+// console.log(merhaba);
+// console.log(selam);
+
+// var instructor ='Mark'
+// var d = `Merhaba ${instructor}`
+// console.log(d);
+
+// console.log(`${2+3}`);
+
+console.log(Boolean(" "));
+var s;
+console.log(Boolean(s));
+console.log(Boolean(null));
+
+console.log(typeof s);
+console.log(typeof null);
+
+
