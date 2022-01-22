fce06b10897276d233153462d91a926e4e9b91eb
mevo <mevlutodabas7@gmail.com>
2022-01-22 17:08:14 +0100

    dersler

---
+++ App.js
@@ -0,0 +1,19 @@
+
+// var x = 999999999999999;
+// var y = 9999999999999999;
+// console.log(x);
+// console.log(y);
+
+// var x =0.1 +0.2
+// console.log(x);
+// console.log(+x.toFixed(5));
+
+// console.log(15+25);
+//  console.log(015+025);
+//  // octal 01234567 :( 5 + 8 =13) +(5 +16 =21) 13+21=34
+//  console.log(018+028);// decimal
+//  console.log(Number(018));
+
+var a = 2;
+var b = a;
+console.log({a},{b});
