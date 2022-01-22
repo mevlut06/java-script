a889b2133090e9ac7f12e696e0b65910b9b61214
mevo <mevlutodabas7@gmail.com>
2022-01-22 13:03:12 +0100

    cumartesi ders notlari

---
+++ workss/operator.js
@@ -0,0 +1,77 @@
+// var x =2;
+// var y = 5;
+// var z = 8;
+
+// var t ='2';
+
+// console.log(x+y);
+// console.log(typeof (x+t))
+
+//Increment decrement
+// var a = 10;
+// console.log(a++);
+// console.log(a);
+
+
+// var b= 5;
+// console.log(b);
+// console.log(++b);
+// console.log(++b);
+// console.log(b);
+
+
+// var c = 8;
+
+// // console.log(c--);
+// // console.log(c);
+
+// // // var d = c++
+// // var d = ++c
+
+// // console.log({d},{c});
+// console.log(c++ +  ++c);
+// console.log(c);
+
+// var input = +prompt("Enter Age");
+// console.log(input, typeof input);
+
+// var c = '8';
+
+// // console.log(-c);
+// var a = 4;
+// var b = '4';
+// // var c = a==b;
+// // console.log(c);
+// // console.log(a===b);
+
+// console.log(a !== b);
+
+// var a = 'a';
+// var b = 'A';
+// console.log(a.charCodeAt());
+// console.log(b.charCodeAt());
+//  console.log(b > a);
+
+// console.log('Fullstack' == 'FullStack');
+
+
+// var a = (true || false);
+// var a = (0 || 3)
+// console.log(a);
+
+// 
+// var capitalLetter = true;
+// var symbol = false;
+// var passLength = true;
+
+// var result = capitalLetter && symbol && passLength;
+// console.log(result || ('Try Again'));
+
+var a;
+console.log(a);
+console.log(a ?? 'Null veya undefined');
+var b = null;
+console.log(b ?? a);
+
+var c = 1;
+console.log(c ?? 'Null veya undefined');
