250022625aac25cd4c59a7a0f9e82e470bb29db0
mevo <mevlutodabas7@gmail.com>
2022-01-26 21:45:27 +0100

    while loops

---
+++ css ders caloismalari/loops.js
@@ -0,0 +1,28 @@
+// let  i  = 10;
+// let sum = 0;
+
+// while (i < 6){
+//     sum+= i
+//     i++;
+// }
+
+// console.log(sum);
+
+// do {
+//     sum+= i
+//     i++;
+// }
+// while (i < 6);
+
+// console.log(sum);
+
+
+let i = +prompt('Bir sayi giriniz :')
+
+
+while (isNaN(i)) {
+    i = +prompt('Bir sayi giriniz :')
+ 
+}
+
+   console.log('Bir sayi girdiniz....');
