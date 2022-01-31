311248275b0d1c4efa65ff1c797cb8da4e5d7dff
mevo <mevlutodabas7@gmail.com>
2022-01-31 22:01:21 +0100

    java 2 ders

---
+++ 002-google-landing-page/java2.js
@@ -0,0 +1,57 @@
+
+
+// let str1 = "Hello"
+// let str2 = "World"
+// let str3 = `${str1+str2}`
+
+// console.log(typeof str1, str2);
+// console.log(typeof str2, str2);
+// console.log(typeof str3, str3);
+
+
+// let str2 = "World"
+// let str4 = new String("A string object")
+
+// console.log(str4);
+// console.log(str2);
+
+// var s1 = "Hello";
+// var s2 = "World!";
+// var s3 = s1.concat(s2);
+// console.log(s3);
+// console.log(s3.concat(' 2022 ', s1));
+// console.log((s1), (s2));
+
+// var a = 'primitive.\nlerin \tproperti veya metodu olmaz.'
+
+
+// console.log(a);
+// console.log(a.charAt());
+// console.log(a.charAt(9));
+// console.log(a.charAt(10));
+// console.log(a.charAt(11));
+// console.log(a.length);
+// console.log(a[a.length-1]);
+
+// var str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+
+// var n = str.includes("simply");
+// var n1 = str.includes("Simply");
+// var n2 = str.includes("simp")
+// console.log(n);
+// console.log(n1);
+// console.log(n2);
+
+var str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+
+// var n = str.indexOf("simply")
+
+// console.log(n);
+// console.log(str.indexOf('xa'));
+// console.log(str.indexOf("Simply"));
+
+// console.log(str.indexOf('m'));
+
+// console.log(str.lastIndexOf('m'));
+
+
