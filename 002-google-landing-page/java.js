3cbce35f4d012ccc076e94ac1fe19b79aec9a8d4
mevo <mevlutodabas7@gmail.com>
2022-01-31 21:00:03 +0100

    java

--- 002-google-landing-page/java.js
+++ 002-google-landing-page/java.js
@@ -57,12 +57,68 @@
 
 // console.log(bill([10,15,20],0.18))
 
-const bill = function(tax, ...list){
-    let total = 0;
-    for(let i =0; i < list.length; i++){
-        total += list[i]+list[i]*tax;
-    }
-    return total;
-}
+// const bill = function(tax, ...list){
+//     let total = 0;
+//     for(let i =0; i < list.length; i++){
+//         total += list[i]+list[i]*tax;
+//     }
+//     return total;
+// }
 
-console.log(bill(0.18,10,15,20,10))
+// console.log(bill(0.18,10,15,20,10))
+
+// function bolme (num1,num2){
+//     if(num2 === 0)return "Zero Division Error"
+//     9return num1 / num2
+// }
+// console.log(bolme(12,2));
+// console.log(bolme(12,0));
+
+// function div2(num1, num2){
+//     return num2 ? num1 / num2 : 'Zero division Error'
+// }
+// console.log(div2(10,3));
+
+// function pascalNumber1(n){
+//     let sum = 0;
+//     for(let i = 1; i < n+1; i++){
+//     sum += i
+//     }
+//     return  sum
+// }
+//  console.log(pascalNumber1(100)); 
+
+// function pascalNumber2(n){
+//     return (n* (n+1)/2)
+// }
+// console.log(pascalNumber2(100));
+
+// function pascalNumber3 (n){
+//     if(n === 1) return 1;
+//     return n + pascalNumber3(n-1)
+// }
+// console.log(pascalNumber3(5));
+
+// function pascalNumber4(n) {
+//     return n === 1 ? 1 : n + pascalNumber4(n-1);
+// }
+// console.log(pascalNumber4(5));
+
+// function add100(a){
+//     return a + 100;
+// }
+// // function expression
+// let toplam = function (a){return a + 100}
+
+// Arrow Function
+
+// let ahmet = (a) => {
+//     return a + 100
+// }
+
+let mehmet = (a) => a + 100
+
+console.log(mehmet(50));
+
+let toplam = a => a + 100;
+console.log(toplam(30));
